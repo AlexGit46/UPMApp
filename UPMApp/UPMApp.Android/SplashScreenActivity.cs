a477adb0b13a00b9f9ac430bdb2a1a598747586b
Alejandro Alba Ramírez <46890323+AlexGit46@users.noreply.github.com>
2019-09-19 16:46:16 +0200

    Agregar archivos de proyecto.

---
+++ UPMApp/UPMApp.Android/SplashScreenActivity.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+
+namespace UPMApp.Droid
+{
+    [Activity(Label = "SplashScreenActivity", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        async void SimulateStartup()
+        {
+            await Task.Delay(2000); 
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
